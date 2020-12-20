89bcdf1e1087ed44e6fed5ec637598ca6460674b
Dion <ksundong@gmail.com>
2020-12-20 22:35:46 +0900

    feat: 실패하는 테스트 작성

---
+++ src/test/java/dev/idion/iloveyouboss/ScoreCollectionTest.java
@@ -0,0 +1,13 @@
+package dev.idion.iloveyouboss;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class ScoreCollectionTest {
+
+  @Test
+  void test() {
+    fail("Not Implemented");
+  }
+}
