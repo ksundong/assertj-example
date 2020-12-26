1d6a63838c2f7549adc55615542f2fa10e02c9e6
Dion <ksundong@gmail.com>
2020-12-26 10:55:06 +0900

    test: arithmeticMean이 성공하는 테스트 코드 작성

--- src/test/java/dev/idion/iloveyouboss/ScoreCollectionTest.java
+++ src/test/java/dev/idion/iloveyouboss/ScoreCollectionTest.java
@@ -1,13 +1,22 @@
 package dev.idion.iloveyouboss;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
 class ScoreCollectionTest {
 
   @Test
-  void test() {
-    fail("Not Implemented");
+  void answersArithmeticMeanOfTwoNumbers() {
+    // 준비
+    ScoreCollection collection = new ScoreCollection();
+    collection.add(() -> 5);
+    collection.add(() -> 7);
+
+    // 실행
+    int actualResult = collection.arithmeticMean();
+
+    // 단언
+    assertThat(actualResult).isEqualTo(6);
   }
 }
